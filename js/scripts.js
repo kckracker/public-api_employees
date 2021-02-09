a61f27c290e88abfab9b3a8938bab1a7e2a91c2c
kckracker <caseyholland972@gmail.com>
2021-02-08 20:43:53 -0500

    Naming update
    Update student based naming sequence to employee to better represent the website directory.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@
 const searchContainer = document.querySelector('.search-container');
 // Sourced syntax and functionality of Request object from https://developer.mozilla.org/en-US/docs/Web/API/Request/Request
 // CORS added as mode value due to initial fetch issues with missing Allow-Control-Allow-Origin header on API.
-const randomStudent = new Request("https://randomuser.me/api/?results=12&&nat=us", {
+const randomEmployee = new Request("https://randomuser.me/api/?results=12&&nat=us", {
     mode: "cors"
 });
-const studentGallery = document.querySelector('#gallery');
-const students = [];
-let studentCards = [];
+const employeeGallery = document.querySelector('#gallery');
+const employees = [];
+let employeeCards = [];
 let modalWindows = [];
 let names = [];
 
@@ -33,22 +33,22 @@ searchContainer.insertAdjacentHTML('beforeend', `<form action="#" method="get">
 
 /**
  * Function to handle fetching API and pushing data to DOM
- * @param {url} url - Accepts API url to pull random student data, parse data to JSON, 
+ * @param {url} url - Accepts API url to pull random employee data, parse data to JSON, 
  * push data to DOM, and add Event Listeners to DOM elements.
- * - Use Promise.all to call addStudent and addListeners functions with collective promise data.
- * - Add event listeners after addStudent function to ensure all DOM elements have been added.
+ * - Use Promise.all to call addEmployee and addListeners functions with collective promise data.
+ * - Add event listeners after addEmployee function to ensure all DOM elements have been added.
  */
 
-async function fetchStudent(url){
+async function fetchEmployee(url){
     await fetch(url)
     .then(response => response.json())
     .then(data => {
-        students.push(data.results);
+        employees.push(data.results);
     });
-    await Promise.all(students)
+    await Promise.all(employees)
     .then((data) => {
-        addStudent(data);
-        addListeners(studentCards);
+        addEmployee(data);
+        addListeners(employeeCards);
     })
     
 }
@@ -57,23 +57,23 @@ async function fetchStudent(url){
 
 /**
  * Function handling generation of DOM elements and distribution of API data to the newly formed DOM elements.
- * @param {array} array Accepts array of JSON objects retrieved from student API stored at randomStudent
+ * @param {array} array Accepts array of JSON objects retrieved from employee API stored at randomEmployee
  */
-function addStudent(arrays){
+function addEmployee(arrays){
     // Ensure innerHTML of gallery div is reset upon refresh of page
-    studentGallery.innerHTML = ``;
+    employeeGallery.innerHTML = ``;
     // Loop through each element of supplied JSON data and inserting HTML from supplied values in JSON data
     for(let array of arrays){
-        for(let student of array){
-            studentGallery.insertAdjacentHTML('beforeend', `
+        for(let employee of array){
+            employeeGallery.insertAdjacentHTML('beforeend', `
             <div class="card">
                 <div class="card-img-container">
-                    <img class="card-img" src=${student.picture.medium} alt="profile picture">
+                    <img class="card-img" src=${employee.picture.medium} alt="profile picture">
                 </div>
                 <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${student.name.first} ${student.name.last}</h3>
-                    <p class="card-text">${student.email}</p>
-                    <p class="card-text cap">${student.location.city}</p>
+                    <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="card-text">${employee.email}</p>
+                    <p class="card-text cap">${employee.location.city}</p>
                 </div>
             </div>
         `)
@@ -87,14 +87,14 @@ function addStudent(arrays){
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src=${student.picture.large} alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${student.name.first} ${student.name.last}</h3>
-                        <p class="modal-text">${student.email}</p>
-                        <p class="modal-text cap">${student.location.city}</p>
+                        <img class="modal-img" src=${employee.picture.large} alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                        <p class="modal-text">${employee.email}</p>
+                        <p class="modal-text cap">${employee.location.city}</p>
                         <hr>
-                        <p class="modal-text">${student.phone.replace(/^(\(\d{3}\))-(\d{3}-\d{4})$/, '$1 $2')}</p>
-                        <p class="modal-text">${student.location.street.number} ${student.location.street.name}, ${student.location.city}, ${student.location.state} ${student.location.postcode}</p>
-                        <p class="modal-text">Birthday: ${student.dob.date.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/, '$2/$3/$1')}</p>
+                        <p class="modal-text">${employee.phone.replace(/^(\(\d{3}\))-(\d{3}-\d{4})$/, '$1 $2')}</p>
+                        <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                        <p class="modal-text">Birthday: ${employee.dob.date.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/, '$2/$3/$1')}</p>
                     </div>
                 </div>
                 <div class="modal-btn-container">
@@ -106,17 +106,17 @@ function addStudent(arrays){
         }
     }
     /*
-    - Pushing newly created student card objects into empty studentCards array for use with DOM manipulation in helper function.
+    - Pushing newly created employee card objects into empty employeeCards array for use with DOM manipulation in helper function.
     - Pushing newly created modal windows into separate array for use with DOM manipulation in helper function.
     - Pushing card names into separate array to use as validator in search helper function
     */
-    studentCards.push(document.querySelectorAll('div.card'));
+    employeeCards.push(document.querySelectorAll('div.card'));
     modalWindows.push(document.querySelectorAll('.modal-container'));
     names.push(document.querySelectorAll('#name.card-name'));
 }
 
 /**
- * Filters student display based on search input for names searched
+ * Filters employee display based on search input for names searched
  * @param {input} - Accepts value of user input, looping over names array to determine if input matches a name
  * - If a match is found or input is empty:
  *      - Matching name cards are displayed
@@ -128,7 +128,7 @@ function addStudent(arrays){
  *      -  Hidden display for no matches is set to display
  */
 
-function searchStudent(input){
+function searchEmployee(input){
     let found = 0;
     for(let i = 0; i <names.length; i++){
         for(let j = 0; j < names[i].length; j++){
@@ -149,15 +149,15 @@ function searchStudent(input){
 
 /**
  * - Helper function loops through modal windows array,
- *       displaying hidden content matching array index of clicked student card
- * @param {number} counter - Accepts number of array index of student cards clicked on by user
+ *       displaying hidden content matching array index of clicked employee card
+ * @param {number} counter - Accepts number of array index of employee cards clicked on by user
  *  - Loops through each modal window in DOM:
- *      - Applies event listener on previous button to display previous student if clicked and hide current.
+ *      - Applies event listener on previous button to display previous employee if clicked and hide current.
  *          - If previous window is first in array, previous button is hidden from window.
- *      - Applies identical listener to next button to display next student if clicked and hide current.
+ *      - Applies identical listener to next button to display next employee if clicked and hide current.
  *          - If next window is last in array, next button is hidden from display.
  *      - Applies event listener on close button to return hidden status to modal window if clicked.
- *      - If first student card is clicked, prev button will not display. If last student card is clicked, next button will not display.
+ *      - If first employee card is clicked, prev button will not display. If last employee card is clicked, next button will not display.
  *  
  */
 function viewModal(counter){
@@ -199,24 +199,24 @@ function viewModal(counter){
 
 /**
  * Function inserting event listeners for search function and modal window toggling
- * @param {array} - Accepts array of studentCards to allow listeners to apply and affect all student cards displayed
+ * @param {array} - Accepts array of employeeCards to allow listeners to apply and affect all employee cards displayed
  * - Loops through array pushed as parameter: 
- *      - Adds keyup listener to search input field and calls searchStudent helper function, supplying user input
- *      - Adds click listener to search button and calls searchStudent helper function, supplying user input from input field
+ *      - Adds keyup listener to search input field and calls searchEmployee helper function, supplying user input
+ *      - Adds click listener to search button and calls searchEmployee helper function, supplying user input from input field
  *      - Loops through each array index of the array supplied: 
- *          - Adds click listener to each student card and calls viewModal helper funtion, supplying index of student card in array.
+ *          - Adds click listener to each employee card and calls viewModal helper funtion, supplying index of employee card in array.
  */
 function addListeners(array){
     // Creating reference to search DOM elements for ease of reference and inserting here because elements are created dynamically.
     const searchInput = document.querySelector('#search-input');
     const searchButton = document.querySelector('#search-submit');
     for (let i = 0; i < array.length; i++){
-        //Calling event listener for search bar and button inside loop of student cards
+        //Calling event listener for search bar and button inside loop of employee cards
         searchInput.addEventListener('keyup', (e) => {
-            searchStudent(searchInput.value);
+            searchEmployee(searchInput.value);
         })
         searchButton.addEventListener('click', () => {
-            searchStudent(searchInput.value);
+            searchEmployee(searchInput.value);
         })
         
         for (let j = 0; j < array[i].length; j++){
@@ -233,8 +233,8 @@ function addListeners(array){
 
 //***** FUNCTION CALL & TIMEOUT *****//
 /*
-- Calling fetchStudent function, supplying reference to Request object variable randomStudent.
+- Calling fetchEmployee function, supplying reference to Request object variable randomEmployee.
 - Adding window timeout to ensure fetch has time to retrieve all necessary data for DOM manipulation.
 */ 
-fetchStudent(randomStudent);
+fetchEmployee(randomEmployee);
 window.setTimeout(1000);
